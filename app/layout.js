62c328dfbed7cc8cc69378b7a7f2c67065e3b422
g33571-gif <g33571@gmail.com>
2026-04-26 20:07:34 +0900

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -21,6 +21,9 @@ export default function RootLayout({ children }) {
           href="https://fonts.googleapis.com/css2?family=Instrument+Serif:ital@0;1&family=Pretendard:wght@400;500;600;700&family=JetBrains+Mono:wght@400;500&display=swap"
           rel="stylesheet"
         />
+            {/* 구글 애드센스 */}
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1705565687896529"
+          crossOrigin="anonymous"></script>
       </head>
       <body>{children}</body>
     </html>
